7d4551a8531f44228a0b7d5907e2c0b0f68f497e
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-07 13:59:32 +0000

    añade clase NivelTriaje

--- lib/pr07.rb
+++ lib/pr07.rb
@@ -1,4 +1,5 @@
-require "pr07/version"
+require_relative "pr07/version"
+require_relative "pr07/nivel_triaje"
 
 module Pr07
   class Error < StandardError; end
---
+++ lib/pr07/nivel_triaje.rb
@@ -0,0 +1,7 @@
+class NivelTriaje
+  attr_reader :nivel
+
+  def initialize(nivel)
+    @nivel = nivel
+  end
+end
