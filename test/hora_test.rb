d5ea85accdd09d255ddb46e1e294741401e21200
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-07 14:48:32 +0000

    test sobre inicialización de la clase Hora

---
+++ test/hora_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class TestHora < Test::Unit::TestCase
+  def setup
+    @hora1 = Hora.new(12, 34, 54)
+  end
+
+  def test_initialize
+    assert_equal(12, @hora1.hora, "debería recuperar el valor 12 de las horas")
+    assert_equal(34, @hora1.minuto, "debería recuperar el valor 34 de los minutos")
+    assert_equal(54, @hora1.segundo, "debería recuperar el valor 54 de los segundos")
+  end
+end
