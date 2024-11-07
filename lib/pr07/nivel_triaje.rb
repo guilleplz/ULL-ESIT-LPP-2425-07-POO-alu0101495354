84b1458bb7880a5c8d72cd25a64dc28632d5acd6
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-07 14:24:27 +0000

    corrige errores

--- lib/pr07/nivel_triaje.rb
+++ lib/pr07/nivel_triaje.rb
@@ -7,12 +7,13 @@ class NivelTriaje
 
   def nivel_prioridad
     case @nivel
-    when 1 then "Reanimación"
-    when 2 then "Emergencia"
-    when 3 then "Urgente"
-    when 4 then "Menos urgente"
-    when 5 then "No urgente"
-    else "Desconocido"
+      when 1 then "Reanimacion"
+      when 2 then "Emergencia"
+      when 3 then "Urgente"
+      when 4 then "Menos urgente"
+      when 5 then "No urgente"
+      else "Desconocido"
+    end
   end
 
 end
