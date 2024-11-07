0df3c74b7bbf4b3a617036bb3e425ba55a9d8234
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-07 14:57:39 +0000

    añade test diferencia de hora

--- test/hora_test.rb
+++ test/hora_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class TestHora < Test::Unit::TestCase
   def setup
     @hora1 = Hora.new(12, 34, 54)
+    @hora2 = Hora.new(10, 30, 45)
+    @hora3 = Hora.new(12, 45, 55)
+    @hora4 = Hora.new(10, 31, 10)
 
   end
 
@@ -15,5 +18,28 @@ class TestHora < Test::Unit::TestCase
   def test_initialize_fuera_rango
     assert_raises(ArgumentError) { Hora.new(25, 10, 30) }
   end
+
+  def test_diferencia_en_tiempo
+
+
+    # Calcular la diferencia de tiempo
+    diferencia = @hora2.diferencia_en_tiempo(@hora3)
+
+    # Verificar que la diferencia es correcta
+    assert_equal({ horas: 2, minutos: 15, segundos: 10 }, diferencia)
+  end
+
+  def test_diferencia_en_tiempo_con_segundos_negativos
+    # Crear dos instancias de Hora donde los segundos sean negativos
+
+
+    # Calcular la diferencia de tiempo
+    diferencia = @hora2.diferencia_en_tiempo(@hora4)
+
+    # Verificar que la diferencia es correcta
+    assert_equal({ horas: 0, minutos: 0, segundos: 25 }, diferencia)
+  end
+
+
 
 end
