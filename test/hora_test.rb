cffe18ecbb82231443ef5467b7c31cff8bbf1355
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-07 14:51:59 +0000

    test con hora fuera de su rango

--- test/hora_test.rb
+++ test/hora_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class TestHora < Test::Unit::TestCase
   def setup
     @hora1 = Hora.new(12, 34, 54)
+
   end
 
   def test_initialize
@@ -10,4 +11,9 @@ class TestHora < Test::Unit::TestCase
     assert_equal(34, @hora1.minuto, "debería recuperar el valor 34 de los minutos")
     assert_equal(54, @hora1.segundo, "debería recuperar el valor 54 de los segundos")
   end
+
+  def test_initialize_fuera_rango
+    assert_raises(ArgumentError) { Hora.new(25, 10, 30) }
+  end
+
 end
