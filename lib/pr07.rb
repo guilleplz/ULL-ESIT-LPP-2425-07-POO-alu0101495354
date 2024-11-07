852a0a6c2642e1d8e5bd72aa6339fa07c10eda9d
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-07 14:48:14 +0000

    crea la clase hora

--- lib/pr07.rb
+++ lib/pr07.rb
@@ -1,5 +1,7 @@
 require_relative "pr07/version"
 require_relative "pr07/nivel_triaje"
+require_relative "pr07/constants"
+require_relative "pr07/hora"
 
 module Pr07
   class Error < StandardError; end
--- lib/pr07/hora.rb
+++ lib/pr07/hora.rb
@@ -2,6 +2,15 @@ class Hora
   attr_reader :hora, :minuto, :segundo
 
   def initialize(hora, minuto, segundo)
+    # Validar que la hora esté entre 0 y 23
+    raise ArgumentError, "La hora debe estar entre 0 y 23" unless hora.between?(0, 23)
+
+    # Validar que los minutos estén entre 0 y 59
+    raise ArgumentError, "Los minutos deben estar entre 0 y 59" unless minuto.between?(0, 59)
+
+    # Validar que los segundos estén entre 0 y 59
+    raise ArgumentError, "Los segundos deben estar entre 0 y 59" unless segundo.between?(0, 59)
+
     @hora = hora
     @minuto = minuto
     @segundo = segundo
