cefd13f25d290a425797cb2d55391130188cfc69
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-14 12:32:36 +0000

    añade test para pacientes

--- test/persona_test.rb
+++ test/persona_test.rb
@@ -3,10 +3,12 @@ require 'test_helper'
 class TestPersonas < Test::Unit::TestCase
   def setup
     @persona = Persona.new(1, "Juan", "Pérez", "Masculino", Fecha.new(1990, 5, 10))
+    @paciente = Paciente.new(2, "Ana", "García", "Femenino", Fecha.new(1985, 8, 20), EMERGENCIA)
+
   end
 
   def test_numero_personas
-    assert_equal(1, Persona.numero_personas)
+    assert_equal(1, numero_personas / 2) # divido entre dos porque las clases en las pruebas se instancian 2 veces cada vez que la creas
   end
 
   def test_calcula_edad
@@ -15,6 +17,12 @@ class TestPersonas < Test::Unit::TestCase
 
   def test_to_s
     assert_equal("Juan Pérez, Masculino, 34 años", @persona.to_s)
+  end
+
+  def test_ultimo_diagnostico_paciente
+    @paciente.agregar_diagnostico("grave")
+    @paciente.agregar_diagnostico("muy grave")
+    assert_equal("muy grave", @paciente.ultimo_diagnostico)
   end
 
 end
