51ae723e8a554c4d0d137707a2c0f3f1846db18a
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-14 11:26:04 +0000

    añade la clase persona

---
+++ lib/pr07/personas/persona.rb
@@ -0,0 +1,39 @@
+class Persona
+  attr_reader :id, :sexo
+
+  @@personas_instanciadas = 0
+
+  def initialize(id, nombre, apellido, sexo, fecha_nacimiento)
+    @id = id
+    @sexo = sexo
+    @fecha_nacimiento = fecha_nacimiento
+    @nombre = nombre
+    @apellido = apellido
+    @@personas_instanciadas += 1
+  end
+
+  def self.numero_personas
+    @@personas_instanciadas
+  end
+
+  def nombre_completo
+    "#{@nombre} #{@apellido}"
+  end
+
+  def edad
+    fecha_actual = Fecha.new(Time.now.year, Time.now.month, Time.now.day)
+    fecha_actual.diferencia_anios(@fecha_nacimiento)
+  end
+
+  def to_s
+    "#{nombre_completo}, #{sexo}, #{edad} años"
+  end
+
+  protected
+
+  attr_reader :fecha_nacimiento
+
+  private
+
+  attr_reader :nombre, :apellido
+end
--- lib/pr07/version.rb
+++ lib/pr07/version.rb
@@ -1,3 +1,3 @@
 module Pr07
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 end
