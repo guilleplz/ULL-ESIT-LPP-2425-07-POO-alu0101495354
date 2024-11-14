763c6c841081d2b7236ad94e540a6817142e16a3
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-14 12:51:19 +0000

    test clase titular carga maxima

--- test/persona_test.rb
+++ test/persona_test.rb
@@ -5,7 +5,7 @@ class TestPersonas < Test::Unit::TestCase
     @persona = Persona.new(1, "Juan", "Pérez", "Masculino", Fecha.new(1990, 5, 10))
     @paciente = Paciente.new(2, "Ana", "García", "Femenino", Fecha.new(1985, 8, 20), EMERGENCIA)
     @medico = Medico.new(3, "Laura", "Martínez", "Femenino", Fecha.new(1975, 3, 15), "Pediatría")
-
+    @titular = Titular.new(4, "Carlos", "López", "Masculino", Fecha.new(1980, 10, 25), "Geriatría", 3)
   end
 
   # def test_numero_personas
@@ -31,11 +31,23 @@ class TestPersonas < Test::Unit::TestCase
     assert_equal(1, @medico.numero_pacientes)
 
 
-    paciente1 = Paciente.new(5, "Pedro", "Sánchez", "Masculino", Time.new(1992, 1, 1), "Nivel III")
-    paciente2 = Paciente.new(6, "María", "Hernández", "Femenino", Time.new(1994, 2, 2), "Nivel IV")
+    paciente1 = Paciente.new(5, "Pedro", "Sánchez", "Masculino", Time.new(1992, 1, 1), REANIMACION)
+    paciente2 = Paciente.new(6, "María", "Hernández", "Femenino", Time.new(1994, 2, 2), NO_URGENTE)
     @medico.agregar_paciente(paciente1)
     @medico.agregar_paciente(paciente2)
     assert_equal(3, @medico.numero_pacientes)
   end
+
+  def test_carga_maxima_titular
+    paciente1 = Paciente.new(7, "Luis", "Ramos", "Masculino", Time.new(1988, 3, 3), NO_URGENTE)
+    paciente2 = Paciente.new(8, "Eva", "Domínguez", "Femenino", Time.new(1990, 4, 4), EMERGENCIA)
+    paciente3 = Paciente.new(9, "Sonia", "Vargas", "Femenino", Time.new(1985, 5, 5), REANIMACION)
+    @titular.agregar_paciente(paciente1)
+    @titular.agregar_paciente(paciente2)
+    @titular.agregar_paciente(paciente3)
+    assert(@titular.carga_maxima?)
+  end
+
+
 
 end
