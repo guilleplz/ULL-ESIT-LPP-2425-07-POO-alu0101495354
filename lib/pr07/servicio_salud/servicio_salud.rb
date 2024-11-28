b8aca1ac9a979d6485440531ef297ee9d8b0c9a0
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-28 14:46:19 +0000

    añade comparable y enumerable a ServicioSalud

--- lib/pr07/servicio_salud/servicio_salud.rb
+++ lib/pr07/servicio_salud/servicio_salud.rb
@@ -1,4 +1,7 @@
 class ServicioSalud
+  include Comparable
+  include Enumerable
+
   attr_accessor :codigo_identificacion, :descripcion, :horario_apertura, :horario_cierre,
                 :calendario_festivos, :medicos, :camas_estandar
 
@@ -26,6 +29,25 @@ class ServicioSalud
     raise 'Cama no ocupada' unless @camas_estandar[cama_id]
 
     (fecha_alta.diferencia_dias(fecha_ingreso))
+  end
+
+  def ==(other)
+    return nil unless other.is_a?(ServicioSalud)
+    self.codigo_identificacion == other.codigo_identificacion &&
+    self.descripcion == other.descripcion &&
+    self.horario_apertura == other.horario_apertura &&
+    self.horario_cierre == other.horario_cierre &&
+    self.calendario_festivos == other.calendario_festivos &&
+    self.medicos == other.medicos &&
+    self.camas_estandar == other.camas_estandar
+  end
+
+  def <=>(other)
+    @medicos.size <=> other.medicos.size
+  end
+
+  def each
+    @medicos.each { |medico| yield medico }
   end
 
 end
