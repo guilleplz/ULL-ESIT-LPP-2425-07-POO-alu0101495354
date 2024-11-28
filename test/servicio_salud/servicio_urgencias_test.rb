c84f6a32ab4ec6a2dcee6a0804b49303efa6d943
Guillermo Plaza <alu0101495354@ull.edu.es>
2024-11-28 15:24:56 +0000

    test función numero de camas totales

--- test/servicio_salud/servicio_urgencias_test.rb
+++ test/servicio_salud/servicio_urgencias_test.rb
@@ -3,10 +3,12 @@ require 'test_helper'
 class ServicioUrgenciasTest < Test::Unit::TestCase
 
   def setup
+
     @servicio = ServicioUrgencias.new(1, "hospital", Hora.new(8, 0, 0), Hora.new(20, 0, 30), [Fecha.new(2024, 1, 1), Fecha.new(2024, 12, 6)], 10)
     @paciente = Paciente.new(1, "Juan", "García", "Hombre", Fecha.new(1980, 1, 1), 1)
     @medico = Medico.new(1, "Pedro", "López", "Hombre", Fecha.new(1970, 1, 1), "12345678A")
     @servicio.medicos << @medico
+    @servicio.asignar_paciente_a_cama(@paciente, 1)
   end
 
   def test_servicio_urgencias
@@ -18,5 +20,8 @@ class ServicioUrgenciasTest < Test::Unit::TestCase
     assert_equal [@medico], @servicio.medicos
     assert_equal 10, @servicio.camas_uci_disponibles
   end
-  
+
+  def test_numero_camas_libres
+    assert_equal 11, @servicio.numero_camas_libres
+  end
 end
